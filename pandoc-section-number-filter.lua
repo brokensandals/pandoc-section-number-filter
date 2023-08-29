5e7d75a3f18cff5c628d3299080fd4862cf19d5f
Jacob Williams <jacobaw@gmail.com>
2023-08-28 21:47:33 -0700

    initial import

---
+++ pandoc-section-number-filter.lua
@@ -0,0 +1,14 @@
+local current_by_level = {0, 0, 0, 0}
+
+function Header(hdr)
+  for i=hdr.level+1,#current_by_level do
+    current_by_level[i] = 0
+  end
+  current_by_level[hdr.level] = current_by_level[hdr.level] + 1
+  secnum = ""
+  for i=1,hdr.level do
+    secnum = secnum .. tostring(current_by_level[i]) .. "."
+  end
+  table.insert(hdr.content, 1, secnum .. " ")
+  return hdr
+end
